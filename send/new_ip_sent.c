043cf94f0fa3c257a0f213f450a6fd8156727fa3
deeptadevkota <deeptadevkota21@gmail.com>
2022-08-18 19:44:25 +0530

    send/receive

--- send/new_ip_sent.c
+++ send/new_ip_sent.c
@@ -38,18 +38,18 @@ int main()
 
     struct ifreq ifreq_i;
     memset(&ifreq_i, 0, sizeof(ifreq_i));
-    strncpy(ifreq_i.ifr_name, "wlp2s0", IFNAMSIZ - 1);
+    strncpy(ifreq_i.ifr_name, "enp0s3", IFNAMSIZ - 1);
     printf("%s\n", ifreq_i.ifr_name);
     if ((ioctl(sock_raw, SIOCGIFINDEX, &ifreq_i)) < 0) // getting the the Interface index
         printf("error in index ioctl reading");
     else
         printf("index = %d\n", ifreq_i.ifr_ifindex);
 
-    struct ifreq ifreq_c;
-    memset(&ifreq_c, 0, sizeof(ifreq_c));
-    strncpy(ifreq_c.ifr_name, "wlp2s0", IFNAMSIZ - 1);
-    if ((ioctl(sock_raw, SIOCGIFHWADDR, &ifreq_c)) < 0) // getting MAC Address
-        printf("error in SIOCGIFHWADDR ioctl reading");
+    // struct ifreq ifreq_c;
+    // memset(&ifreq_c, 0, sizeof(ifreq_c));
+    // strncpy(ifreq_c.ifr_name, "enp0s3", IFNAMSIZ - 1);
+    // if ((ioctl(sock_raw, SIOCGIFHWADDR, &ifreq_c)) < 0) // getting MAC Address
+    //     printf("error in SIOCGIFHWADDR ioctl reading");
 
     // struct ifreq ifreq_ip;
     // memset(&ifreq_ip, 0, sizeof(ifreq_ip));
@@ -59,36 +59,36 @@ int main()
     //     printf("error in SIOCGIFADDR\n");
     // }
 
-    unsigned char *sendbuff = (unsigned char *)malloc(64); // increase in case of more data
-    memset(sendbuff, 0, 64);
-    struct ethhdr *eth = (struct ethhdr *)(sendbuff);
-    eth->h_source[0] = (unsigned char)(ifreq_c.ifr_hwaddr.sa_data[0]);
-    eth->h_source[1] = (unsigned char)(ifreq_c.ifr_hwaddr.sa_data[1]);
-    eth->h_source[2] = (unsigned char)(ifreq_c.ifr_hwaddr.sa_data[2]);
-    eth->h_source[3] = (unsigned char)(ifreq_c.ifr_hwaddr.sa_data[3]);
-    eth->h_source[4] = (unsigned char)(ifreq_c.ifr_hwaddr.sa_data[4]);
-    eth->h_source[5] = (unsigned char)(ifreq_c.ifr_hwaddr.sa_data[5]);
+    // char *sendbuff = (unsigned char *)malloc(64); // increase in case of more data
+    // memset(sendbuff, 0, 64);
+    // struct ethhdr *eth = (struct ethhdr *)(sendbuff);
+    // eth->h_source[0] = (unsigned char)(ifreq_c.ifr_hwaddr.sa_data[0]);
+    // eth->h_source[1] = (unsigned char)(ifreq_c.ifr_hwaddr.sa_data[1]);
+    // eth->h_source[2] = (unsigned char)(ifreq_c.ifr_hwaddr.sa_data[2]);
+    // eth->h_source[3] = (unsigned char)(ifreq_c.ifr_hwaddr.sa_data[3]);
+    // eth->h_source[4] = (unsigned char)(ifreq_c.ifr_hwaddr.sa_data[4]);
+    // eth->h_source[5] = (unsigned char)(ifreq_c.ifr_hwaddr.sa_data[5]);
 
-    eth->h_dest[0] = DESTMAC0;
-    eth->h_dest[1] = DESTMAC1;
-    eth->h_dest[2] = DESTMAC2;
-    eth->h_dest[3] = DESTMAC3;
-    eth->h_dest[4] = DESTMAC4;
-    eth->h_dest[5] = DESTMAC5;
+    // eth->h_dest[0] = DESTMAC0;
+    // eth->h_dest[1] = DESTMAC1;
+    // eth->h_dest[2] = DESTMAC2;
+    // eth->h_dest[3] = DESTMAC3;
+    // eth->h_dest[4] = DESTMAC4;
+    // eth->h_dest[5] = DESTMAC5;
 
-    eth->h_proto = htons(0x88b6);
+    // eth->h_proto = htons(0x88b6);
     // printf("%d\n",eth->h_proto);
-    total_len += sizeof(struct ethhdr);
+    // total_len += sizeof(struct ethhdr);
 
-    struct new_ip *new_iph = (struct new_ip *)(sendbuff + sizeof(struct ethhdr));
-    new_iph->a = 1;
-    total_len += sizeof(struct new_ip);
+    // struct new_ip *new_iph = (struct new_ip *)(sendbuff + sizeof(struct ethhdr));
+    // new_iph->a = 1;
+    // total_len += sizeof(struct new_ip);
 
-    sendbuff[total_len++] = 0xAA;
-    sendbuff[total_len++] = 0xBF;
-    sendbuff[total_len++] = 0xCC;
-    sendbuff[total_len++] = 0xDD;
-    sendbuff[total_len++] = 0xEE;
+    // sendbuff[total_len++] = 0xAA;
+    // sendbuff[total_len++] = 0xBF;
+    // sendbuff[total_len++] = 0xCC;
+    // sendbuff[total_len++] = 0xDD;
+    // sendbuff[total_len++] = 0xEE;
 
     // struct sockaddr_ll sadr_ll;
     // sadr_ll.sll_ifindex = ifreq_i.ifr_ifindex;
@@ -107,14 +107,14 @@ int main()
     //     return -1;
     // }
 
+    char ip[] = {"Test message. Test message. Test message!\n"};
+    // ip.a = 12345;
+    printf("%s\n",ip);
 
-    struct new_ip ip;
-    ip.a = 12345;
-
-    size_t msg_len = sizeof(ip); 
+    size_t msg_len = strlen(ip) + 1;
 
     struct iovec iov[1];
-    iov[0].iov_base = &ip;
+    iov[0].iov_base = ip;
     iov[0].iov_len = msg_len;
 
     struct sockaddr_ll sadr_ll;
@@ -136,8 +136,7 @@ int main()
     message.msg_control = 0;
     message.msg_controllen = 0;
 
-
-    printf("%ld\n",sendmsg(sock_raw, &message, 0));
+    printf("%ld\n", sendmsg(sock_raw, &message, 0));
     // if (sendmsg(sock_raw, &message, 0) == -1)
     // {
     //     printf("Error in sending the message!");
